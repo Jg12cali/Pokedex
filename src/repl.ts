3aeae9a746ce3bc8b99183019b21620a68c18964
Jg12cali <juangranados862@yahoo.com>
2026-03-03 19:29:48 -0500

    JG: Working draft

--- src/repl.ts
+++ src/repl.ts
@@ -1,9 +1,41 @@
+import { createInterface } from "readline"
+
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'Pokedex >'
+  })
+
+
+
+export function startREPL(){
+
+rl.prompt()
+
+rl.on("line", (input => { 
+
+    if(!input) {
+        rl.prompt()
+        return
+        
+
+
+}
+
+const result = cleanInput(input)
+
+console.log(`Your command was: ${result[0]}`)
+rl.prompt()
+
+
+}))}
+
+
+
 export function cleanInput (input: string): string[] {
 
  let cleaned  = input.toLowerCase().trim().split(' ').filter(item => item !== '')
  return cleaned
 
 }
-
-
 
